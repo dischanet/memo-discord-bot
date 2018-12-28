d0f6554a91c997274c1fdb80db162bdc042d669a
mouse_484 <escmouse484@gmail.com>
2018-12-28 17:07:21 +0900

    メモの文字数等問題の回避

--- files/commands/add.js
+++ files/commands/add.js
@@ -6,7 +6,19 @@ module.exports.set = {
 //コマンド内容
 module.exports.run = (db,client, message) => {
   let memo = message.content.split(' ');
+  let memo_title = memo[1];
+  let memo_content = memo[2];
   if (memo.legnth < 3) {
-    message.channel.send('正しく入力してください。\nメモを追加するには\n!madd [タイトル] [内容]');
+    message.channel.send('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
+    return;
   }
+  if (memo_title > 10) {
+    message.channel.send('タイトルが長すぎます。\n10文字以内に変更してください。');
+    return;
+  }
+  if (memo_content > 1000) {
+    message.channel.send('内容が長すぎます。\n1000文字以内に変更してください。');
+    return;
+  }
+  
 };

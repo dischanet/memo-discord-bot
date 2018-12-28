aa96767f42c4d95d6e405e239495eeecad71b964
mouse_484 <escmouse484@gmail.com>
2018-12-28 18:02:30 +0900

    lengthをtypoしてた

--- files/commands/add.js
+++ files/commands/add.js
@@ -8,17 +8,16 @@ module.exports.run = (db,client, message) => {
   let memo = message.content.split(' ');
   let memo_title = memo[1];
   let memo_content = memo[2];
-  if (memo.legnth < 3) {
+  if (memo.length < 3) {
     message.channel.send('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
     return;
   }
-  if (memo_title > 10) {
+  if (memo_title.length > 10) {
     message.channel.send('タイトルが長すぎます。\n10文字以内に変更してください。');
     return;
   }
-  if (memo_content > 1000) {
+  if (memo_content.length > 1000) {
     message.channel.send('内容が長すぎます。\n1000文字以内に変更してください。');
     return;
-  }
-  
+  }  
 };

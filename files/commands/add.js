a3ec46330fd6faadf5aa1a38bade935d58dc5364
mouse_484 <escmouse484@gmail.com>
2018-12-29 10:32:18 +0900

    うまく追加できるようにした
    (ネストヤバみ)

--- files/commands/add.js
+++ files/commands/add.js
@@ -4,20 +4,58 @@ module.exports.set = {
 };
 
 //コマンド内容
-module.exports.run = (db,client, message) => {
-  let memo = message.content.split(' ');
-  let memo_title = memo[1];
-  let memo_content = memo[2];
-  if (memo.length < 3) {
-    message.channel.send('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
-    return;
-  }
-  if (memo_title.length > 10) {
-    message.channel.send('タイトルが長すぎます。\n10文字以内に変更してください。');
-    return;
-  }
-  if (memo_content.length > 1000) {
-    message.channel.send('内容が長すぎます。\n1000文字以内に変更してください。');
-    return;
-  }  
+module.exports.run = (db, client, message) => {
+
+  db.serialize(() => {
+
+    let memo = message.content.split(' ');
+    let memo_title = memo[1];
+
+    if (memo.length < 3) {
+      message.channel.send('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
+      return;
+    }
+    if (memo_title.length > 10) {
+      message.channel.send('タイトルが長すぎます。\n10文字以内に変更してください。');
+      return;
+    }
+    memo.splice(0, 2);
+    let memo_content = memo.join(' ');
+    if (memo_content.length > 1000) {
+      message.channel.send('内容が長すぎます。\n1000文字以内に変更してください。');
+      return;
+    }
+
+    db.get('SELECT * FROM memo WHERE user_id=? AND title=?;', [message.author.id, memo_title], (err, row) => {
+      if (row) {
+        message.channel.send('同じタイトルのメモが登録されています。\n削除してもう一度登録してください。');
+        return;
+      } else {
+        db.run('INSERT INTO memo VALUES(?,?,?,?)', [message.author.id, message.author.username, memo_title, memo_content], (err_ins) => {
+          if (!err_ins) {
+            let embed = {
+              'title': 'メモを追加しました',
+              'color': 0x4A90E2,
+              'fields': [
+                {
+                  'name': 'タイトル',
+                  'value': memo_title,
+                },
+                {
+                  'name': '内容',
+                  'value': memo_content,
+                }
+              ],
+              'footer': {
+                'icon_url': message.author.avatarURL,
+                'text': message.author.tag,
+              },
+            };
+            message.channel.send({ embed });
+          }
+        });
+      }
+    });
+  });
+
 };

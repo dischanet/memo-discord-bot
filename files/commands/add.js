ef9b171d4ab4af3f39ec555b5bcee3f875fe0edd
mouse_484 <escmouse484@gmail.com>
2018-12-29 10:54:22 +0900

    メモの個数制限の追加

--- files/commands/add.js
+++ files/commands/add.js
@@ -4,33 +4,38 @@ module.exports.set = {
 };
 
 //コマンド内容
-module.exports.run = (db, client, message) => {
-
+module.exports.run = async (db, client, message) => {
+  let msg = await message.channel.send('メモの書き込み確認をしています...');
   db.serialize(() => {
 
     let memo = message.content.split(' ');
     let memo_title = memo[1];
 
     if (memo.length < 3) {
-      message.channel.send('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
+      msg.edit('正しく入力してください。\nメモを追加するには`!madd [タイトル] [内容]`です。');
       return;
     }
     if (memo_title.length > 10) {
-      message.channel.send('タイトルが長すぎます。\n10文字以内に変更してください。');
+      msg.edit('タイトルが長すぎます。\n10文字以内に変更してください。');
       return;
     }
     memo.splice(0, 2);
     let memo_content = memo.join(' ');
     if (memo_content.length > 1000) {
-      message.channel.send('内容が長すぎます。\n1000文字以内に変更してください。');
+      msg.edit('内容が長すぎます。\n1000文字以内に変更してください。');
       return;
     }
 
-    db.get('SELECT * FROM memo WHERE user_id=? AND title=?;', [message.author.id, memo_title], (err, row) => {
-      if (row) {
-        message.channel.send('同じタイトルのメモが登録されています。\n削除してもう一度登録してください。');
+    db.all('SELECT * FROM memo WHERE user_id=?', [message.author.id], (err, rows) => {
+      if (rows.length > 100) {
+        msg.edit('メモが大すぎます。(100個)\n不要なメモを削除してください。');
         return;
-      } else {
+      }
+      db.get('SELECT * FROM memo WHERE user_id=? AND title=?;', [message.author.id, memo_title], (err_t, row) => {
+        if (row) {
+          msg.edit('同じタイトルのメモが登録されています。\n削除してもう一度登録してください。');
+          return;
+        }
         db.run('INSERT INTO memo VALUES(?,?,?,?)', [message.author.id, message.author.username, memo_title, memo_content], (err_ins) => {
           if (!err_ins) {
             let embed = {
@@ -51,10 +56,10 @@ module.exports.run = (db, client, message) => {
                 'text': message.author.tag,
               },
             };
-            message.channel.send({ embed });
+            msg.edit({ embed });
           }
         });
-      }
+      });
     });
   });
 

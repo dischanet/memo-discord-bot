20bb899a2b5f40f8c60d507512f8ec29c54eea21
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-04 22:33:29 +0900

    [修正]コンフリクト解決こぼれを修正

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-<<<<<<< HEAD
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('memo.db');
-=======
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("memo.db");
->>>>>>> upstream/master
 db.serialize(() => {
   db.run(
     "CREATE TABLE IF NOT EXISTS memo(user_id TEXT, name TEXT, title TEXT, content TEXT);"

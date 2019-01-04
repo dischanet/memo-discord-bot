d9c80ed06f8b540184e083b6416328fbd40acb06
mouse_484 <escmouse484@gmail.com>
2019-01-04 15:45:59 +0900

    適切なメゾット(someからforEach)に変更

--- files/command.js
+++ files/command.js
@@ -3,7 +3,7 @@ const fs = require('fs');//ファイル操作用モジュール
 module.exports.run = (client, message, prefix, db) => {
 
   fs.readdir('files/commands/', (err, files) => {//コマンドの読み込み
-    files.some((file) => {
+    files.forEach((file) => {
 
       const aliases = [];
       const command = require(`./commands/${file}`);

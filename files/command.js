b09938453f28a223103034d3b541b4b7f82e544d
mouse_484 <escmouse484@gmail.com>
2019-01-04 16:31:05 +0900

    !mコマンド(表示)を使えるようにするために一部変更

--- files/command.js
+++ files/command.js
@@ -1,15 +1,25 @@
 const fs = require('fs');//ファイル操作用モジュール
-
+const show = require('./commands/show.js');
 module.exports.run = (client, message, prefix, db) => {
 
-  fs.readdir('files/commands/', (err, files) => {//コマンドの読み込み
+  if (message.content.startsWith(`${prefix} `)) {
+
+    show.run(db, client, message);
+    return;
+
+  }
+
+  fs.readdir('files/commands/', (err, files) => {
     files.forEach((file) => {
 
       const command = require(`./commands/${file}`);
 
       if (command.set.aliases.includes(message.content.slice(prefix.length).split(' ')[0])) {
-        command.run(db,client, message);
+
+        command.run(db, client, message);
+
       }
+      
     });
   });
 };

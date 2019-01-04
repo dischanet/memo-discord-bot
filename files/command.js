1e298400a5b7aa8fe11091feec80dd52b23da080
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-04 19:59:25 +0900

    [修正]prettierの追加と.eslintの設定

--- files/command.js
+++ files/command.js
@@ -1,14 +1,17 @@
-const fs = require('fs');//ファイル操作用モジュール
+const fs = require("fs"); //ファイル操作用モジュール
 
 module.exports.run = (client, message, prefix, db) => {
-
-  fs.readdir('files/commands/', (err, files) => {//コマンドの読み込み
+  fs.readdir("files/commands/", (err, files) => {
+    //コマンドの読み込み
     files.forEach((file) => {
-
       const command = require(`./commands/${file}`);
 
-      if (command.set.aliases.includes(message.content.slice(prefix.length).split(' ')[0])) {
-        command.run(db,client, message);
+      if (
+        command.set.aliases.includes(
+          message.content.slice(prefix.length).split(" ")[0]
+        )
+      ) {
+        command.run(db, client, message);
       }
     });
   });
--- files/commands/test.js
+++ files/commands/test.js
@@ -1,9 +1,9 @@
 module.exports.set = {
-  name: 'test',//名前
-  aliases: ['test'],//エイリアス
+  name: "test", //名前
+  aliases: ["test"], //エイリアス
 };
 
 //コマンド内容
 module.exports.run = (db, client, message) => {
-  message.channel.send('テストです');
+  message.channel.send("テストです");
 };

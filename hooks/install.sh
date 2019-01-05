5638ea7d9cc7626c998f40a0743b30da677ca0b3
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-05 17:40:30 +0900

    developブランチ用のボット作成

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,8 +2,12 @@
 
 cd /home/ec2-user/repos/memo-discord-bot
 
-REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 PARAMETER_NAME=MEMO_DISCORD_BOT_SECRET
+if [ "$DEPLOYMENT_GROUP_NAME" == "develop" ]
+then
+    PARAMETER_NAME=MEMO_DEVELOP_DISCORD_BOT_SECRET
+fi
+REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameter --name ${PARAMETER_NAME} --query "Parameter.Value" --output text)" > environment
 
 cp ./hooks/memo-discord-bot.service /etc/systemd/system/memo-discord-bot.service
